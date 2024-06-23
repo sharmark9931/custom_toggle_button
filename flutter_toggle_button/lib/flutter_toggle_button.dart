fb2b978c354b5b004a07aeac6e231d2c43267048
Ravi Kumar Sharma <53875332+sharmark9931@users.noreply.github.com>
2024-06-23 05:44:00 +0530

    Delete flutter_toggle_button directory

--- flutter_toggle_button/lib/flutter_toggle_button.dart
+++
@@ -1,9 +0,0 @@
-/// A customizable toggle button widget for Flutter.
-///
-/// This library provides a `CustomToggleButton` widget that allows for flexible
-/// and customizable toggle buttons. The buttons can be styled with different
-/// colors, gradients, border radii, and more.
-
-library custom_toggle_button;
-
-export 'src/toggle_button_widget.dart';
